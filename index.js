a711a0133b593cd4eac5347f7e1256a1f107e506
MursalinDevOps <mursalin1768@gmail.com>
2024-12-31 23:47:40 +0600

    added endpoint to create a new item data in the database

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Track n trace is on the right track and has trace :)')
+app.get("/", (req, res) => {
+  res.send("Track n trace is on the right track and has trace :)");
 });
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mzx0h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -32,7 +32,26 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    const database = client.db("lostItemsDB");
+    const itemsCollection = database.collection("items");
+    //
+    // Endpoint to create a new item data in the database
+    app.post("/add-items", async (req, res) => {
+      const addItemFormData = req.body;
+    //   console.log(addItemFormData);
+      const result = await itemsCollection.insertOne(addItemFormData);
+      res.send(result);
+    });
+
+    // app.get("/add-items", async(req, res)=>{
+    //     const cursor = itemsCollection.find();
+    //     const result = await cursor.toArray();
+    //     res.send(result);
+    // })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -40,8 +59,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.listen(port, ()=>{
-    console.log(`Server is running on ${port} port`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on ${port} port`);
+});
