342585289bacd47cfcbdbaa12c36cfaa6589761a
MursalinDevOps <mursalin1768@gmail.com>
2025-01-02 14:03:32 +0600

    added endpoint to create api for a single item by ID

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Track n trace is on the right track and has trace :)");
@@ -40,19 +40,27 @@ async function run() {
     const itemsCollection = database.collection("items");
     //
     // Endpoint to create a new item data in the database
-    app.post("/add-items", async (req, res) => {
+    app.post("/all-items", async (req, res) => {
       const addItemFormData = req.body;
-    //   console.log(addItemFormData);
+      //   console.log(addItemFormData);
       const result = await itemsCollection.insertOne(addItemFormData);
       res.send(result);
     });
 
-    
-    app.get("/add-items", async(req, res)=>{
-        const cursor = itemsCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    // Endpoint to create api for all items
+    app.get("/all-items", async (req, res) => {
+      const cursor = itemsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Endpoint to create api for a single item by ID
+    app.get("/all-items/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await itemsCollection.findOne(query);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
